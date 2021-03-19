3eaf38d7c49c3847e732c600870b3744596e1275
VictorSpini <80986891+VictorSpini@users.noreply.github.com>
2021-03-19 18:35:13 -0300

    Delete Semana 02 - Python - Victor Spini directory

--- Semana 02 - Python - Victor Spini/exerc11.py
+++
@@ -1,8 +0,0 @@
-# How to Set the Path and Switch Between Different Versions/Executables (Windows)
-
-import sys
-
-print(sys.version)
-print(sys.executable)
-
-# End
--- Semana 02 - Python - Victor Spini/exerc12.py
+++
@@ -1,7 +0,0 @@
-# How to Set the Path and Switch Between Different Versions/Executables (Mac & Linux)
-import sys
-
-print(sys.version)
-print(sys.executable)
-
-# End
